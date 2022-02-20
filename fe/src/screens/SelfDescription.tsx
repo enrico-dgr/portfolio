165db3663b2b673269282d75ef2e6905e030c12a
Enrico <enrico.digrazia98@gmail.com>
2022-02-20 22:14:00 +0100

    Text

--- fe/src/screens/SelfDescription.tsx
+++ fe/src/screens/SelfDescription.tsx
@@ -20,9 +20,16 @@ class SelfDescription extends Component {
 				<Suspense fallback={<Html>Loading...</Html>}>
 					<Html
 						fullscreen
-						style={{ color: "white", fontSize: 20, fontWeight: "bold" }}
+						style={{
+							alignItems: "start",
+							color: "white",
+							display: "flex",
+							flexDirection: "column",
+							fontWeight: "bold",
+							justifyContent: "flex-start",
+						}}
 					>
-						<div style={{ paddingLeft: 50, paddingTop: 30 }}>
+						<div style={{ fontSize: 20, paddingLeft: 50, paddingTop: 30 }}>
 							<p>Currently improving my skills in web development</p>
 							<p>at React Academy in Beije.</p>
 							<p>Inscribed to the Faculty of Physics in Catania,</p>
@@ -30,6 +37,26 @@ class SelfDescription extends Component {
 							<p>Before Beije Academy, I studied as a self-taught,</p>
 							<p>learning basics of both back-end and front-end.</p>
 							<p>Now I'm focusing on front-end.</p>
+						</div>
+						<div
+							style={{
+								alignItems: "end",
+								display: "flex",
+								flexDirection: "column",
+								fontSize: 18,
+								justifyContent: "start",
+								marginTop: 40,
+								width: "100%",
+							}}
+						>
+							<div style={{ paddingRight: 40, textAlign: "right" }}>
+								<p style={{ fontSize: 20 }}>
+									If you're curious about the technologies used for this site,
+								</p>
+								<p>for now it is highly dependent on...</p>
+								<p>Typescript -- React (.tsx) -- react-router-dom</p>
+								<p>@react-three/fiber -- @react-spring/three</p>
+							</div>
 						</div>
 					</Html>
 				</Suspense>
