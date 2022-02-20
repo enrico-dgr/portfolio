bc0b3cd64378005b169691a1e2bcb28f211b3e86
Enrico <enrico.digrazia98@gmail.com>
2022-02-20 21:00:15 +0100

    Cube animation

--- fe/src/screens/Portfolio/Background.tsx
+++ fe/src/screens/Portfolio/Background.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from "react";
-import { animated } from "@react-spring/three";
+import { animated, useSpring } from "@react-spring/three";
 import { Box3, Mesh, MeshStandardMaterial, Vector3 } from "three";
 import { MeshProps, useFrame } from "@react-three/fiber";
 
@@ -15,24 +15,34 @@ const Background = (props: MeshProps) => {
 	// states
 	const [maxColumns, _setMaxColumns] = useState<number>(30);
 	const [radius, _setRadius] = useState<number>(0.02);
+	const [cubeHover, setCubeHover] = useState(false);
+	const switchHover = () => setCubeHover(!cubeHover);
+
+	// springs
+	const springsCube = useSpring({
+		color: cubeHover ? "blue" : "red",
+	});
 
 	// refs
 	const ref = useRef<Mesh>(null);
+	const refSquare = useRef<Mesh>(null);
 	const [dotsRefs, addRef] = useRefs<Mesh>(600, null);
 
 	// frames
-	useFrame(({ mouse, scene, viewport }) => {
+	useFrame(({ clock, mouse, scene, viewport }) => {
 		const sceneBox = new Box3().setFromObject(scene);
 		const sceneSizes = new Vector3().copy(sceneBox.max).sub(sceneBox.min);
 
 		if (!!ref.current) {
 			const halfSizes = sceneSizes.divideScalar(2);
+			// center background
 			ref.current.position.set(
 				scene.position.x - halfSizes.x,
 				scene.position.y + halfSizes.y,
 				ref.current.position.z
 			);
 
+			// reusable instances
 			const mousePos = new Vector3(
 				(mouse.x * viewport.width) / 2,
 				(mouse.y * viewport.height) / 2,
@@ -40,6 +50,7 @@ const Background = (props: MeshProps) => {
 			);
 			const dotPos = new Vector3();
 
+			// dots animations
 			dotsRefs.forEach((dot) => {
 				if (!!dot.current) {
 					const mouseDistance = mousePos.distanceToSquared(
@@ -71,11 +82,46 @@ const Background = (props: MeshProps) => {
 					}
 				}
 			});
+
+			// square animation
+			if (!!refSquare.current) {
+				clock;
+
+				const initPos = new Vector3(
+					scene.position.x + halfSizes.x * 1.5,
+					scene.position.y - halfSizes.y,
+					0
+				);
+
+				const elaps = clock.getElapsedTime();
+				const x = Math.cos(elaps * 1.8) * halfSizes.x * 0.25;
+				const y = Math.sin(elaps * 1.2) * halfSizes.y * 0.45;
+
+				refSquare.current.position.set(initPos.x + x, initPos.y - y, 0);
+				refSquare.current.rotation.set(elaps, elaps, -elaps);
+			}
 		}
 	});
 
 	return (
 		<mesh {...props} ref={ref}>
+			<animated.mesh
+				onPointerEnter={switchHover}
+				onPointerLeave={switchHover}
+				ref={refSquare}
+			>
+				<boxGeometry args={[0.6, 0.6, 0.6]} />
+				<meshStandardMaterial
+					attach="material"
+					color={springsCube.color.get()}
+					emissive={"blue"}
+					emissiveIntensity={2}
+					metalness={0.7}
+					opacity={0.6}
+					roughness={0.1}
+					transparent={true}
+				/>
+			</animated.mesh>
 			{dotsRefs.map((_, i) => (
 				<animated.mesh
 					key={`portofolio-bg ${i}`}
