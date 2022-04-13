428f5bb78eb27bbf3147bc4a316baf780bb1c341
enrico-dgr <enrico.digrazia98@gmail.com>
2022-04-13 14:24:31 +0200

    nothing changed

--- fe/specificComponent/index.tsx
+++ fe/specificComponent/index.tsx
@@ -2,10 +2,9 @@ import React from "react";
 import * as ReactDOM from "react-dom";
 
 // drei stats
-import { Stats } from "@react-three/drei";
+// import { Stats } from "@react-three/drei";
 
 // component
-import NavSphere from "../src/components/classComponents/navSphere/NavSphere";
 
 // style
 import "./style.scss";
@@ -13,10 +12,4 @@ import "./style.scss";
 let domContainer = document.getElementById("react");
 if (domContainer === null) throw new Error("dom container is null.");
 
-ReactDOM.render(
-	<div>
-		<Stats showPanel={0} />
-		<NavSphere />
-	</div>,
-	domContainer
-);
+ReactDOM.render(<div>{/* <Stats showPanel={0} /> */}</div>, domContainer);
